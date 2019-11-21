a4f265b81e3720398c079e7427d0c1e43d67f319
Thirunarayanan Balathandayuthapani <thiru@mariadb.com>
2019-11-21 13:41:39 +0530

    MDEV-16678 Use MDL for innodb background threads instead of dict_operation_lock
    - To avoid the crash in fts_optimize_callback() if timer initiated
    callback comes after FTS_MSG_STOP.

--- storage/innobase/fts/fts0opt.cc
+++ storage/innobase/fts/fts0opt.cc
@@ -2835,18 +2835,18 @@ void fts_optimize_callback(
 /*================*/
 	void*		arg)			/*!< in: work queue*/
 {
-	static ulint		current = 0;
-	static ibool		done = FALSE;
-	static ulint		n_tables = ib_vector_size(fts_slots);
-	static ulint		n_optimize = 0;
-	ib_wqueue_t*		wq = fts_optimize_wq;
-
 	ut_ad(!srv_read_only_mode);
 
 	if (!fts_optimize_wq) {
 		/* Possibly timer initiated callback, can come after FTS_MSG_STOP.*/
 		return;
 	}
+
+	static ulint		current = 0;
+	static ibool		done = FALSE;
+	static ulint		n_tables = ib_vector_size(fts_slots);
+	static ulint		n_optimize = 0;
+	ib_wqueue_t*		wq = fts_optimize_wq;
 
 	while (!done && srv_shutdown_state == SRV_SHUTDOWN_NONE) {
 
@@ -3038,7 +3038,7 @@ fts_optimize_shutdown()
 	os_event_destroy(fts_opt_shutdown_event);
 	ib_wqueue_free(fts_optimize_wq);
 	fts_optimize_wq = NULL;
-
+	fts_opt_thd = NULL;
 }
 
 /** Sync the table during commit phase
